d54d2d01f559fe578fd44d58d27f29b2db6c96d8
Maksudul <maaksuduldish@gmail.com>
2025-03-17 16:43:36 +0600

    Layout setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='max-w-6xl mt-10 mx-auto'>
+      <div className='text-center mb-10'>
+        <h1 className='text-4xl font-bold'>Chef's Table with ReactJS</h1>
+        <p className='text-sm font-semibold text-gray-400 mt-3'>Explore our all recipes</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
